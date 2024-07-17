c0d4a61069933d4a8a33482193abd310ce76b109
Subhangill <m.subhansafdar786@gmail.com>
2024-07-17 23:15:40 +0500

    Update Agecalculator.js

--- Agecalculator/Agecalculator.js
+++ Agecalculator/Agecalculator.js
@@ -1,23 +1,24 @@
-function calculateAge(){
-    let day = parseInt(document.getElementById('day').value);
-    let month = parseInt(document.getElementById('month').value)-1;
-    let year = parseInt(document.getElementById('year').value);
+function  calculateAge(){
+    const day = parseInt(document.getElementById('day').value);
+    const month = parseInt(document.getElementById('month').value)-1;
+    const year = parseInt(document.getElementById('year').value);
 
-    let dateofBitrth = new Date(year , month , day);
-    let today = new Date();
+    const dateofBirth = new Date(year , month , day);
+    const  today = new Date();
 
-    const ageYear = today.getFullYear() - dateofBitrth.getFullYear();
-    const ageMonth = today.getMonth() - dateofBitrth.getMonth();
-    const ageDate = today.getDate() - dateofBitrth.getDate();
+    let ageYear = today.getFullYear()- dateofBirth.getFullYear();
+    let ageMonth = today.getMonth() - dateofBirth.getMonth();
+    let ageDay = today.getDate()-dateofBirth.getDate();
 
-    if(ageDate < 0){
+    if(ageDay < 0 ){
         ageMonth--;
-        ageDate += new Date(today.getFullYear(), today.getMonth , 0).getDate;
-
+        ageDay += new Date ( today.getFullYear() , today.getMonth() , 0).getDate;
     }
-    if (ageMonth < 0){
+     
+    if(ageMonth < 0){
         ageYear--;
         ageMonth += 12;
     }
-    document.getElementById('result').innerHTML= `You are ${ageDate} days , ${ageMonth} months and ${ageYear} years old.`
+
+    document.getElementById('result').innerHTML= `You are ${ageDay} days , ${ageMonth} months and ${ageYear} years old .`;
 }
